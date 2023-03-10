e737dda8cfc37df96c787c3df406fff11f878fe4
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-10 12:30:00 +0530

    Exception

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+      ),
+    );
+  }
+}
